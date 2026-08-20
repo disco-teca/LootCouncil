5f903eaefd0911f041aaa1fd1e22d68ccb46d3bc
Alex M <marseaalex69@gmail.com>
2026-08-20 14:10:43 -0700

    minor cleanup

--- Database.lua
+++ Database.lua
@@ -9,9 +9,6 @@ function LootCouncil.Database:Initialize()
     LootCouncilDB.window.x = LootCouncilDB.window.x or 0
     LootCouncilDB.window.y = LootCouncilDB.window.y or 0
 
-    LootCouncilDB.InspectCache =
-        LootCouncilDB.InspectCache or {}
-
     LootCouncil:Print("Database initialized.")
 
 end
